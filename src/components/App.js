092f77325b31c4924040b4fedaccc94d37b9df8d
Rekha-1 <maneeta808@gmail.com>
2021-04-30 08:52:06 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -6,53 +6,51 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.cityList = [{ name: 'Goa', country: 'India' },
-    { name: 'Amsterdam', country: 'Netherlands' },
+    this.cityList = [
+    // { name: 'Goa', country: 'India' },
+    // { name: 'Amsterdam', country: 'Netherlands' },
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
     { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
+    // { name: 'Brandenburg Gate', country: 'Germany' },
+    // { name: 'Reichstag Building', country: 'Germany' },
+    // { name: 'Museum Island', country: 'Germany' },
+    // { name: 'Munnar', country: 'India' },
+    // { name: 'Leh Ladakh', country: 'India' },
     { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
+    // { name: 'Agra', country: 'India' },
+    // { name: 'Dalhousie', country: 'India' },
+    // { name: 'Coorg', country: 'India' },
+    // { name: 'Rome', country: 'Italy' },
+    // { name: 'Milan', country: 'Italy' },
+    // { name: 'Venice', country: 'Italy' },
+    // { name: 'Varanasai', country: 'India' },
+    // { name: 'Jaipur', country: 'India' },
+    // { name: 'The Hofburg', country: 'Austria' },
+    // { name: 'Belvedere Palace', country: 'Austria' },
+    // { name: 'St. Stephen Cathedral', country: 'Austria' },
+    // { name: 'Kahna National Park', country: 'India' },
+    // { name: 'Amritsar', country: 'India' },
+    // { name: 'Mussoorie', country: 'India' },
+    // { name: 'Mount Abu', country: 'India' },
+    // { name: 'Tirupati', country: 'India' },
     ]
   }
 
   render() {
-    // let filteredData = this.cityList.filter((obj,i) => {
-    //   return obj.country.includes("India");
-    //  })
+    let filteredData = this.cityList.filter((obj,i) => {
+      return obj.country.includes("India");
+     })
     return (
       <div id="main">
         <ol>
-        {/* {
+        {
           filteredData.map((data, i) => {
             console.log(data)
             return <li key={`location${i}`}>{data.name}</li>;
           })
-          } */}
-          <li key="location1">Goa</li>
-          <li key="location2">Darjeeling</li>
-          <li key="location3">Lonavala</li>
+          }
         </ol>
       </div>
     )
